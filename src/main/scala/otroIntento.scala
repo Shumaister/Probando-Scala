5c945711f794aa09c0ba8c41cbb7edfd84f5655e
Gabriel Maiori <gabrielmaiori@gmail.com>
2018-10-04 12:00:13 -0300

    Initial commit scala

---
+++ src/main/scala/otroIntento.scala
@@ -0,0 +1,3 @@
+class otroIntento {
+
+}
---
+++ src/test/scala/WordlTest/testFinally.scala
@@ -0,0 +1,20 @@
+package WordlTest
+
+import org.scalatest.FreeSpec
+
+class testFinally extends FreeSpec{
+
+  "Probar la wea" - {
+
+    "set vacio de test" - {
+      "test truchisimo" in {
+
+      }
+
+      "la suma anda" in {
+        assert(2 == 1+1)
+      }
+    }
+  }
+
+}
